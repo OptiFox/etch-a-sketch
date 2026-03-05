036d36afb95bca08476552374a21710f1e4c295e
OptiFox <80470592+OptiFox@users.noreply.github.com>
2026-03-06 05:26:22 +0800

    Initial commit
    This is the finished project of Etch-a-Sketch from The Odin Project

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# etch-a-sketch
+# The Odin Project: Etch-a-Sketch
+
+## Project Overview
+
+This repository contains my solution for the Etch-a-Sketch project by The Odin Project, which is the toughest one yet for me. I have learned quite a lot from this one, such as how to properly make a grid using CSS, randomizing colors, opacity increment, using JavaScript functions like prompt(), confirm(), etc.
+
+## Features
+
+This page contains the following:
+
+* **Grid:** This is where you can draw in this project.
+* **Configure Grid Button:** You can configure the size of the grid from here by inputting a number, and it will make a grid based on that. (Input: 16, Grid: 16x16)
+* **Clear Grid Button:** You can clear the grid by pressing this button.
+* **Reset Grid Button:** You will be asked for confirmation before resetting to default grid size (16x16).
+
+## Technologies Used
+
+* **HTML5:** For the main page layout.
+* **CSS3:** For the styling and coloring.
+* **JavaScript:** For an interactive web experience.
+
+## Credits
+
+* This is where I find about opacity increment: https://stackoverflow.com/a/12648072
---
+++ script.js
@@ -0,0 +1,85 @@
+const container = document.querySelector(".container");
+let gridSize = 16;
+
+function randomizeColor() {
+    let red = Math.trunc(Math.random() * 255);
+    let green = Math.trunc(Math.random() * 255);
+    let blue = Math.trunc(Math.random() * 255);
+
+    return `rgb(${red} ${green} ${blue})`;
+}
+
+function generateGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const div = document.createElement("div");
+            div.classList.add("item");
+            container.appendChild(div);
+        }
+
+        const br = document.createElement("div");
+        br.classList.add("break");
+
+        container.appendChild(br);
+    }
+
+    const items = document.querySelectorAll(".container .item");
+
+    items.forEach((item) => {
+        item.addEventListener("mouseover", () => {
+            item.style.backgroundColor = randomizeColor();
+
+            let opacity = item.style.opacity;
+            item.style.opacity = opacity ? (parseFloat(opacity) + 0.1) : 0.2;
+        })
+    })
+}
+
+generateGrid(gridSize);
+
+function deleteGrid() {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+const btnConfig = document.querySelector(".config");
+const btnReset = document.querySelector(".reset");
+const btnClear = document.querySelector(".clear");
+
+btnConfig.addEventListener("click", () => {
+    let gridSize = 0;
+
+    do {
+        gridSize = prompt("Enter the size of your grid", "");
+
+        console.log(gridSize);
+
+        if (gridSize === null) {
+            return;
+        }
+
+    } while (gridSize > 100 || gridSize < 1 || isNaN(gridSize));
+
+    if (gridSize > 0 || gridSize <= 100) {
+        deleteGrid();
+        generateGrid(gridSize);
+    }
+});
+
+btnReset.addEventListener("click", () => {
+    let confirmation = confirm("This will reset the grid back to 16x16.\nAre you sure?")
+
+    if (confirmation) {
+        deleteGrid();
+        generateGrid(16);
+    }
+})
+
+btnClear.addEventListener("click", () => {
+    const items = document.querySelectorAll(".container .item");
+
+    for (let item of items) {
+        item.style.backgroundColor = "white";
+    }
+})
